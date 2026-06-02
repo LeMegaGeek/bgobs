e7457efd19d45d4770127fa703404d8544a947c7
LeMegaGeek <d.github@chey.net>
2026-06-02 22:16:47 +0200

    Use prebuilt ONNX Runtime for Windows package
    Signed-off-by: LeMegaGeek <d.github@chey.net>

--- src/DelayLoad.cpp
+++ src/DelayLoad.cpp
@@ -10,19 +10,21 @@
 #include <string>
 
 #include <obs-module.h>
-#include "plugin-support.h"
 
 extern "C" {
+
+extern IMAGE_DOS_HEADER __ImageBase;
 
 FARPROC WINAPI DelayLoadHook(unsigned dliNotify, PDelayLoadInfo pdli)
 {
 	if (dliNotify == dliNotePreLoadLibrary) {
 		const std::string dllName(pdli->szDll);
 		if (dllName == "onnxruntime.dll") {
-			const std::filesystem::path binaryDir("../../obs-plugins/64bit");
+			wchar_t modulePath[MAX_PATH] = {};
+			GetModuleFileNameW(reinterpret_cast<HMODULE>(&__ImageBase), modulePath, MAX_PATH);
 			const std::filesystem::path absPath =
-				std::filesystem::absolute(binaryDir / PLUGIN_NAME / dllName);
-			obs_log(LOG_INFO, "Loading %S from %S", dllName.c_str(), absPath.c_str());
+				std::filesystem::path(modulePath).parent_path() / L"onnxruntime.dll";
+			obs_log(LOG_INFO, "Loading onnxruntime.dll from plugin binary directory");
 			return (FARPROC)LoadLibraryExW(absPath.c_str(), NULL, LOAD_WITH_ALTERED_SEARCH_PATH);
 		} else {
 			return NULL;
